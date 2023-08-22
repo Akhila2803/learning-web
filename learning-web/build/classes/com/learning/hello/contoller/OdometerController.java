367f37b1c87502e2e18b9024a2dc10e8c893c27d
Akhila2803 <141002649+Akhila2803@users.noreply.github.com>
2023-08-22 08:09:17 +0530

    Add files via upload

---
+++ learning-web/build/classes/com/learning/hello/contoller/OdometerController.java
@@ -0,0 +1,31 @@
+package com.learning.hello.contoller;
+
+public class OdometerController {
+    private int currentReading;
+    private int length;
+
+    public OdometerController(int length) {
+        this.length = length;
+        this.currentReading = 0;
+    }
+
+    public int getCurrentReading() {
+        return currentReading;
+    }
+
+    public void setCurrentReading(int reading) {
+        if (reading >= 0 && reading < length) {
+            this.currentReading = reading;
+        } else {
+            System.out.println("Invalid reading. Reading should be within the valid range.");
+        }
+    }
+
+    public void getPreviousReading() {
+        currentReading = (currentReading - 1 + length) % length;
+    }
+
+    public void getNextReading() {
+        currentReading = (currentReading + 1) % length;
+    }
+}
